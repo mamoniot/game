7dfa30de99278bc360218e2e38faabce433fe08b
Monica Moniot <mamoniot@protonmail.com>
2021-10-23 22:00:07 -0400

    attempt

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 glslc w:/code/shaders/shader.vert -o w:/env/shaders/vert.spv -O;
 glslc w:/code/shaders/shader.frag -o w:/env/shaders/frag.spv -O;
-g++ -O3 w:/code/main.cc -o/w:/env/game.exe -I/w:/include -L/w:/lib -lSDL2 -lSDL2main -lvulkan-1 -lVkLayer_utils -Wno-write-strings
+
+pushd w:/dev &> $NULL
+mv w:/env/game.exe previous.exe &> $NULL
+
+cl -nologo -Ox -favor:blend -w w:/code/main.cc -I w:/include -link -incremental:no w:/lib/SDL2.lib w:/lib/SDL2main.lib w:/lib/vulkan-1.lib w:/lib/VkLayer_utils.lib
+
+cp main.exe w:/env/game.exe &> $NULL
+rm main.exe &> $NULL
+popd &> $NULL
